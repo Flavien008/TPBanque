ee0466f536bc405610e49587ee187289d5436c41
Flavien Rakotoarison <72599570+Flavien008@users.noreply.github.com>
2024-04-15 21:03:16 +0300

    creation Bean CDI et backing bean CDI

---
+++ src/main/java/mg/flavien/tpbanqueflavien/config/BeanInit.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.flavien.tpbanqueflavien.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.Initialized;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletContext;
+import mg.flavien.tpbanqueflavien.entities.CompteBancaire;
+import mg.flavien.tpbanqueflavien.service.GestionnaireCompte;
+
+/**
+ *
+ * @author flavi
+ */
+@ApplicationScoped
+public class BeanInit {
+    
+    @Inject    
+    private GestionnaireCompte gestionnaireCompte;
+    
+    public void init(@Observes @Initialized(ApplicationScoped.class) ServletContext context) {
+        CompteBancaire[] compte = new CompteBancaire[4];
+        compte[0] = new CompteBancaire("John Lennon", 150000);
+        compte[1] = new CompteBancaire("Paul McCartney", 950000);
+        compte[2] = new CompteBancaire("Ringo Starr", 20000);
+        compte[3] = new CompteBancaire("Georges Harrisson", 100000);
+        for (CompteBancaire c : compte) {
+            gestionnaireCompte.creerCompte(c);
+        }
+    }
+}
---
+++ src/main/java/mg/flavien/tpbanqueflavien/service/GestionnaireCompte.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.flavien.tpbanqueflavien.service;
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import mg.flavien.tpbanqueflavien.entities.CompteBancaire;
+
+/**
+ *
+ * @author flavi
+ */
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="root",              // nom et
+    password="root", // mot de passe que vous avez donnés lors de la création de la base de données
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true",
+      "driverClass=com.mysql.cj.jdbc.Driver"
+    }
+)
+@ApplicationScoped
+public class GestionnaireCompte {
+    @PersistenceContext(unitName = "banquePU")
+    private EntityManager em;
+    
+    @Transactional
+    public void creerCompte(CompteBancaire c) {
+        em.persist(c);
+    }
+    
+     public List<CompteBancaire> getAllComptes() {
+        Query query = em.createQuery("select c from CompteBancaire as c"); 
+       return query.getResultList();
+    } 
+    
+}
